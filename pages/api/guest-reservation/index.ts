ee3f3284ff94e4951b357ccdc3aac5b02b3cd6f3
Kenny <k6alvarez@gmail.com>
2022-08-24 09:50:39 -0700

    ok this better build

--- pages/api/guest-reservation/index.ts
+++ pages/api/guest-reservation/index.ts
@@ -2,24 +2,21 @@ import { getSession } from 'next-auth/react';
 import prisma from '../../../lib/prisma';
 
 // POST /api/guest-reservation
-// Required fields in body: title
-// Optional fields in body: content
 export default async function handle(req, res) {
   let apiOptions
-  const { name } = req.body;
 
   const session = await getSession({ req });
   if (session?.user?.email) {
     apiOptions = {
       data: {
         author: { connect: { email: session?.user?.email } },
-        name: name,
+        ...req.body,
       },
     }
   } else {
     apiOptions = {
       data: {
-        name: name,
+        ...req.body,
       },
     }
   }
