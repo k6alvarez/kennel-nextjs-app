92ddeec429d234bda6b5862fcb3382182238ef1b
Kenny <k6alvarez@gmail.com>
2022-08-23 17:25:25 -0700

    Update index.ts

--- pages/api/guest-reservation/index.ts
+++ pages/api/guest-reservation/index.ts
@@ -1,7 +1,7 @@
 
-import { getSession } from "next-auth/react";
 import prisma from "../../../lib/prisma";
 
+// POST /api/guest-reservation
 export default async function handle(req, res) {
   const {
     name,
@@ -41,10 +41,14 @@ export default async function handle(req, res) {
     petOneFeedingCount,
   } = req.body;
 
-  const session = await getSession({ req });
+  // Object.entries(req.body).map(([key, value]) => {
+  //   if (INITIAL_USER_STATE[key]?.required && !value ) {
+  //     throw new Error(`${INITIAL_USER_STATE[key]?.label} field is required.`)
+  //   }
+  // })
+
   const result = await prisma.guestReservation.create({
     data: {
-      author: { connect: { email: session?.user?.email } },
       name: name,
       lastName: lastName,
       email: email,
