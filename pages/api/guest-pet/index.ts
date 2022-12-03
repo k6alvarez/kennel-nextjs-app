87a164a214a5308e4b4820561d4bacfd97b45495
Kenny Alvarez <kenny.alvarez@salesloft.com>
2022-12-02 16:21:15 -0800

    Update index.ts

--- pages/api/guest-pet/index.ts
+++ pages/api/guest-pet/index.ts
@@ -13,6 +13,7 @@ export default async function handle(req, res) {
             reservations: { connect: { id: reservationId } },
         },
     });
+    console.log("🚀 ~ file: index.ts:16 ~ handle ~ result", result)
 
     res.json(result);
 }
