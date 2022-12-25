b3a1e78c7be2b1b34ca409015b946f9f85031829
Kenny Alvarez <kenny.alvarez@salesloft.com>
2022-12-25 15:56:55 -0800

    set up selector

--- pages/create-reservation.tsx
+++ pages/create-reservation.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 import { useSession } from "next-auth/react";
 import { Content } from "../components/ui-kit/Base";
 import { GuestClientForm } from "../components/Reservations/GuestClients/GuestClientForm";
 import { ClientForm } from "../components/Reservations/Clients/ClientForm";
+import { ClientStatusSelection } from "../components/Reservations/ClientStatusSelection";
 
 const Reservation: React.FC = () => {
   const { data: session, status } = useSession();
+  const [clientType, setClientType] = useState({
+    clientType: "",
+  });
 
   if (status === "loading") {
     return (
@@ -19,11 +23,36 @@ const Reservation: React.FC = () => {
     );
   }
 
+  if (clientType.clientType === "") {
+    return (
+      <Layout>
+        <Content>
+          <ClientStatusSelection
+            onToggle={(type) => {
+              setClientType({ clientType: type });
+            }}
+            clientType={clientType}
+          />
+        </Content>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
       <Content>
+        <ClientStatusSelection
+          onToggle={(type) => {
+            setClientType({ clientType: type });
+          }}
+          clientType={clientType}
+        />
         <h1>Client Reservations</h1>
-        {!session ? <GuestClientForm /> : <ClientForm session={session} />}
+        {!session || clientType.clientType === "new" ? (
+          <GuestClientForm />
+        ) : (
+          <ClientForm session={session} />
+        )}
       </Content>
     </Layout>
   );
