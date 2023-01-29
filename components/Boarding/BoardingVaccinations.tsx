16414a78f1380a79e4d4c8402e2e8a3a1206d8ca
Kenny Alvarez <kenny.alvarez@salesloft.com>
2023-01-28 22:20:19 -0800

    Update BoardingVaccinations.tsx

--- components/Boarding/BoardingVaccinations.tsx
+++ components/Boarding/BoardingVaccinations.tsx
@@ -37,54 +37,6 @@ export const BoardingVaccinations = ({
       ) : (
         <div dangerouslySetInnerHTML={{ __html: content?.content }} />
       )}
-
-      <FlexCards>
-        <Card title={<h2>Boarding Vaccinations Required</h2>}>
-          <ul>
-            <li>Distemper: Vaccination given within the last one (1) year</li>
-            <li>Parvo-Virus: Vaccination given within the last one (1) year</li>
-            <li>
-              Rabies: Vaccination given within the last one (1) year on dogs
-              under one (1) year of age and within the last three (3) years for
-              dogs over one (1) year of age
-            </li>
-            <li>
-              Bordetella: This is the most important vaccination for dogs being
-              boarded! We absolutely require Bordetella vaccination prior to
-              entry of any dog into our kennel. If you would like more
-              information about kennel cough please see our{" "}
-              <Link href="/boarding?tab=medical-issues">
-                <a>medical issues</a>
-              </Link>{" "}
-              section.
-            </li>
-          </ul>
-        </Card>
-      </FlexCards>
-      <FlexCards>
-        <Card title={<h2>Training Vaccinations Required</h2>}>
-          <ul>
-            <li>Distemper: Vaccination given within the last one (1) year</li>
-            <li>Parvo-Virus: Vaccination given within the last one (1) year</li>
-            <li>
-              Rabies: Vaccination given within the last one (1) year on dogs
-              under one (1) year of age and within the last three (3) years for
-              dogs over one (1) year of age. Because many veterinarians to not
-              give the rabies vaccination to young puppies, rabies is not
-              required for puppy head start.
-            </li>
-            <li>
-              Bordetella: This is the vaccination for kennel cough. It is
-              required for training classes and is for your dog's protection. If
-              you would like more information about kennel cough please see our{" "}
-              <Link href="/boarding?tab=medical-issues">
-                <a>medical issues</a>
-              </Link>{" "}
-              section.
-            </li>
-          </ul>
-        </Card>
-      </FlexCards>
     </Content>
   );
 };
