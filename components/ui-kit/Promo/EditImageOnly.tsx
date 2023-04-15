59d6e5ec88da9bb71c205c91092ac7626ad6c83f
Kenny Alvarez <kenny.alvarez@salesloft.com>
2023-04-15 00:25:08 -0400

    Update EditImageOnly.tsx
    Editmode for slidermode

--- components/ui-kit/Promo/EditImageOnly.tsx
+++ components/ui-kit/Promo/EditImageOnly.tsx
@@ -4,6 +4,11 @@ import message from "antd/lib/message";
 import { StyledInput, StyledLabel } from "../../Forms/styles";
 import { GridItem } from "../Base";
 
+const EditModeContainer = styled.div`
+  background-color: ${(props) => props.theme.colors.white};
+  padding: ${(props) => props.theme.space[3]};
+`;
+
 const Flex = styled.div`
   display: flex;
   flex-direction: column;
@@ -45,58 +50,78 @@ export const EditImageOnly = ({
       {editMode ? (
         <Flex>
           <GridItem size={promo.size} img={promo.image} />
-          <StyledInput
-            type={"file"}
-            name={promo.id}
-            id={promo.id}
-            onChange={async (e) => {
-              e.preventDefault();
-              const file = e.target.files[0];
-              const formData = new FormData();
-              formData.append("file", file);
-              formData.append("upload_preset", "gk-app");
-              setIsLoading(true);
+          <EditModeContainer>
+            <p>Current image url: {promo.image}</p>
+            <label htmlFor="size">Size</label>
+            <select
+              name="size"
+              id="size"
+              value={promo.size}
+              onChange={(e) => updatePromo({ ...promo, size: e.target.value })}
+            >
+              <option value="">Auto</option>
+              <option value="20vw">Small</option>
+              <option value="30vw">Medium</option>
+              <option value="40vw">Large</option>
+            </select>
+            <StyledInput
+              type={"file"}
+              name={promo.id}
+              id={promo.id}
+              onChange={async (e) => {
+                e.preventDefault();
+                const file = e.target.files[0];
+                const formData = new FormData();
+                formData.append("file", file);
+                formData.append("upload_preset", "gk-app");
+                setIsLoading(true);
 
-              message.loading(`Uploading new image.`);
-              const data = await fetch(
-                "https://api.cloudinary.com/v1_1/dhcv2fdfq/image/upload",
-                {
-                  method: "POST",
-                  body: formData,
-                }
-              ).then((res) => res.json());
+                message.loading(`Uploading new image.`);
+                const data = await fetch(
+                  "https://api.cloudinary.com/v1_1/dhcv2fdfq/image/upload",
+                  {
+                    method: "POST",
+                    body: formData,
+                  }
+                ).then((res) => res.json());
 
-              updatePromo && updatePromo({ ...promo, image: data.secure_url });
-              message.success(`New image uploaded.`);
-              setIsLoading(false);
-            }}
-            disabled={isLoading}
-            accept="image/*"
-          />
-          <button
-            type="button"
-            onClick={async () => {
-              setIsLoading(true);
-              const uploadedImage = await fetch(`/api/promo-item/${promo.id}`, {
-                method: "PUT",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                  image: promo.image,
-                }),
-              }).then((res) => res.json());
-              if (uploadedImage.error) {
-                message.error(uploadedImage.error);
+                updatePromo &&
+                  updatePromo({ ...promo, image: data.secure_url });
+                message.success(`New image uploaded.`);
                 setIsLoading(false);
-                return;
-              }
-              message.success(`Image updated.`);
-              setIsLoading(false);
-            }}
-          >
-            Save Image
-          </button>
+              }}
+              disabled={isLoading}
+              accept="image/*"
+            />
+            <button
+              type="button"
+              onClick={async () => {
+                setIsLoading(true);
+                const uploadedImage = await fetch(
+                  `/api/promo-item/${promo.id}`,
+                  {
+                    method: "PUT",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                      image: promo.image,
+                      size: promo.size,
+                    }),
+                  }
+                ).then((res) => res.json());
+                if (uploadedImage.error) {
+                  message.error(uploadedImage.error);
+                  setIsLoading(false);
+                  return;
+                }
+                message.success(`Image updated.`);
+                setIsLoading(false);
+              }}
+            >
+              Save Image
+            </button>
+          </EditModeContainer>
         </Flex>
       ) : (
         <>
